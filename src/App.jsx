aba8bf18ab962cb2d9eb64896db37e9cdf3347d2
gustavoweb <contato@gustavoweb.com>
2025-08-20 10:48:34 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,32 @@ function App() {
           
           {/* Card 1 */}
           <div className="card">
-            <img src={suedAlien1} alt="" />
+            <img src={suedAlien1} alt="" className="img-card"/>
             <h2>Tênis Suede XL Alien</h2>
             <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
             <p className="classificacao"> ★ ★ ★ ★ ☆ (479)</p>
             <p className="preco">R$ 599,99</p>
-            <p className="off">5%</p>
+            <p className="off">- 5%</p>
+          </div>
+
+          {/* Card 2 */}
+          <div className="card">
+            <img src={suedAlien1} alt="" className="img-card"/>
+            <h2>Tênis Suede XL Alien</h2>
+            <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
+            <p className="classificacao"> ★ ★ ★ ★ ☆ (479)</p>
+            <p className="preco">R$ 599,99</p>
+            <p className="off">- 5%</p>
+          </div>
+
+          {/* Card 3 */}
+          <div className="card">
+            <img src={suedAlien1} alt="" className="img-card"/>
+            <h2>Tênis Suede XL Alien</h2>
+            <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
+            <p className="classificacao"> ★ ★ ★ ★ ☆ (479)</p>
+            <p className="preco">R$ 599,99</p>
+            <p className="off">- 5%</p>
           </div>
 
         </section>
