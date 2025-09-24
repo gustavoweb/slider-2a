e231993c5219e06630310de4a09f805fb9ab306d
gustavoweb <contato@gustavoweb.com>
2025-09-24 10:24:35 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import suedAlien1 from './assets/sued-alien-1.avif'
 import LogoPuma from './assets/logo-puma-branco.png'
+import Carmy from './assets/cap-army.jpg'
+import Cjoa from './assets/cap-joaninha.jpg'
+import Curs from './assets/cap-ursinho.avif'
 import LogoInsta from './assets/instagram.svg'
 import LogoDiscord from './assets/discord.svg'
 import LogoWhats from './assets/whatsapp.svg'
@@ -38,46 +41,49 @@ function App() {
             <SwiperSlide>
               <img src={Slider2} alt="" />
             </SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            ...
+            <SwiperSlide>
+              <img src={Slider3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider4} alt="" />
+            </SwiperSlide>
           </Swiper>
         </header>
         <section className="produtos">
           
-          {/* Card 1 */}
+          {/* CARD 1 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={suedAlien1} alt="" className="img-card"/>
-              <h2>Tênis Suede XL Alien</h2>
-              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
-              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
-              <p className="preco">R$ 599,99</p>
-              <p className="off">- 5%</p>
+              <img src={Cjoa} alt="" className="img-card" />
+              <h2 className="titulo-card">Capacete Joaninha</h2>
+              <p className="desc">Proteção ao nível kevlar acoplada ao Estilo Joaninha (Rodolia cardinalis)</p>
+              <p className="preco">R$ 299,99</p>
+              <div className="classificacao"> <span>★ ★ ★ ★ ★</span> (72)</div>
+              <div className="off">- 10%</div>
             </div>
           </a>
 
-          {/* Card 2 */}
+          {/* CARD 2 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={suedAlien1} alt="" className="img-card"/>
-              <h2>Tênis Suede XL Alien</h2>
-              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
-              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
-              <p className="preco">R$ 599,99</p>
-              <p className="off">- 5%</p>
+              <img src={Carmy} alt="" className="img-card" />
+              <h2 className="titulo-card">Capacete PUBG</h2>
+              <p className="desc">Seu galinaceo protegido sem perder o estilo militar dos games online de battle royale.</p>
+              <p className="preco">R$ 399,99</p>
+              <div className="classificacao"> <span>★ ★ ★ ★ ☆</span> (15)</div>
+              <div className="off">- 5%</div>
             </div>
           </a>
 
-          {/* Card 3 */}
+          {/* CARD 3 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={suedAlien1} alt="" className="img-card"/>
-              <h2>Tênis Suede XL Alien</h2>
-              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
-              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
-              <p className="preco">R$ 599,99</p>
-              <p className="off">- 5%</p>
+              <img src={Curs} alt="" className="img-card" />
+              <h2 className="titulo-card">Capacete Urso Ted</h2>
+              <p className="desc">Une proteção, segurança e clarto um dos personagens mais carismáticos do cinema.</p>
+              <p className="preco">R$ 299,99</p>
+              <div className="classificacao"> <span>★ ★ ★ ★ ★</span> (302)</div>
+              <div className="off">- 10%</div>
             </div>
           </a>
 
